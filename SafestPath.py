6152bf4a9930635f00cedb8ec46b71d31da8fca1
junogong <104862416+junogong@users.noreply.github.com>
2025-01-14 19:28:36 -0500

    Update SafestPath.py

--- SafestPath.py
+++ SafestPath.py
@@ -1,6 +1,8 @@
 import arcpy
 from arcpy import env  
 from arcpy.sa import *
+FILE_PATH = "" # The file path containing the input rasters
+
 def buildRangeMap(low, high):
     diff = high - low
     interval = diff / 10
@@ -14,7 +16,7 @@ def reclassifyRasters(rasters, weight):
     for i in range(len(rasters)):
         raster = arcpy.Raster(rasters[i])
         outReclass= Reclassify(raster, "Value", RemapRange(buildRangeMap(raster.minimum, raster.maximum)))
-        outReclass.save("C:\\Users\\Juno\\Downloads\\GGR321 Group Project\\ToolOutput\\reclass_{}.tif".format(rasters[i]))
+        outReclass.save(f"{FILE_PATH}\\reclass_{}.tif".format(rasters[i]))
         timesRaster = arcpy.sa.Times(weight, outReclass)
         times_rasters.append(timesRaster)
     return times_rasters
@@ -28,9 +30,9 @@ def ScriptTool(param0, Start, End, walkRaster):
     sumRaster = makeCostRaster(times_rasters)
     
     finalRaster = arcpy.sa.Plus(walkRaster, sumRaster)
-    finalRaster.save("C:\\Users\\Juno\\Downloads\\GGR321 Group Project\\ToolOutput\\finalCost.tif")
+    finalRaster.save(f"{FILE_PATH}\\finalCost.tif")
     
-    LeastPath = "C:\\Users\\Juno\\Downloads\\GGR321 Group Project\\ToolOutput\\SafestPath.gdb\\SafestPath"
+    LeastPath = f"{FILE_PATH}\\SafestPath.gdb\\SafestPath"
     LeastCostPath = arcpy.intelligence.LeastCostPath(finalRaster, Start, End, LeastPath)
     
     return
@@ -38,7 +40,7 @@ def ScriptTool(param0, Start, End, walkRaster):
 if __name__ == '__main__':
     # Tool parameter accessed with GetParameter or GetParameterAsText
     arcpy.env.overwriteOutput = True
-    arcpy.env.workspace = "C:\\Users\\Juno\\Downloads\\GGR321 Group Project\\ToolOutput\\"
+    arcpy.env.workspace = f"{FILE_PATH}\\ToolOutput\\"
     param0 = arcpy.GetParameterAsText(0)
     param1 = arcpy.GetParameterAsText(1)
     param2 = arcpy.GetParameterAsText(2)
